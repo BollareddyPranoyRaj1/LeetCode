b4b2b20b1da957b691c8ffe2cb49a2d551ed5bd4
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2026-02-18 00:16:18 +0530

    Time: 13 ms (19.87%), Space: 47.2 MB (27.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.remove(nums[i]); 
+            } else {
+                map.put(nums[i], 1);  
+            }
+        }
+        for(int key : map.keySet()){
+            return key;
+        }
+        return -1;
+    }
+}
